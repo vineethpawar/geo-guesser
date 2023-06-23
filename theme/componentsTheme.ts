19c7b4f673637e46756a0f2ca44d237ba00d4534
vineethpawar <vineethpawar99@gmail.com>
2023-06-23 23:50:35 +0530

    chore: add cardRounded variant

--- theme/componentsTheme.ts
+++ theme/componentsTheme.ts
@@ -1,1 +1,11 @@
-export const componentsTheme = {};
+export const componentsTheme = {
+  VStack: {
+    variants: {
+      cardRounded: {
+        rounded: "xl",
+        bg: "cardBG",
+        overflow: "hidden",
+      },
+    },
+  },
+};
